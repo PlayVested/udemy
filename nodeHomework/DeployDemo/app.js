e3e27dd3b57b843deae52271b071618fb9b54a93
Eric Arnold <ClockworkRobot79@users.noreply.github.com>
2018-09-03 21:25:01 -0500

    add console log messages to routes

--- nodeHomework/DeployDemo/app.js
+++ nodeHomework/DeployDemo/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require(`express`);
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set(`view engine`, `ejs`);
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get(`/`, (req, res) => {
+    console.log(`I'm home!`);
+    res.render(`home`);
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get(`/about`, (req, res) => {
+    console.log(`What is this all about?`);
+    res.render(`about`);
 });
 
-const IP = process.env.IP | 'localhost';
-const PORT = process.env.PORT | '1979';
+const IP = process.env.IP | `localhost`;
+const PORT = process.env.PORT | `1979`;
 app.listen(PORT, IP, () => {
     console.log(`listening on ${IP}:${PORT}`);
 });
